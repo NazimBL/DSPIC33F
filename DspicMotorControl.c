19d5d8996bc2ca564b5aa5adc4e8c1586b844373
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-10-29 15:13:43 +0100

    Add files via upload
    Multiple PWM output, using DsPIC33 MC (motor control)

---
+++ DspicMotorControl.c
@@ -0,0 +1,142 @@
+/*
+ * File:   main.c
+ * Author: DELL
+ *
+ * Created on 1 juillet 2018, 10:10
+ */
+
+#include "xc.h"
+
+_FOSCSEL(FNOSC_FRC);
+_FOSC(FCKSM_CSECMD & OSCIOFNC_OFF);
+
+#define FCY 40000000
+#define LED	LATBbits.LATB10
+
+int ADCValue=0;
+char temp;
+void SetupOSC();
+void ADC_Read();
+void ADC_Setup();
+void Timer_Setup();
+void myDelay();
+char received[4];
+float d=1;
+int p1=100,p2=100;
+void PWM_update(int pwm1,int pwm2);
+
+
+int main(void) {
+    
+SetupOSC();
+ADC_Setup();
+PWM_Init();
+
+//LED
+TRISBbits.TRISB10=0;
+LED=0;
+
+while (1) 
+{
+    ADC_Read();
+    //PWM_update(800,1600); test
+  
+}  
+    return 0;
+}
+
+
+
+void ADC_Read(){
+	
+ AD1CON1bits.SAMP = 1;
+ while (!AD1CON1bits.DONE);// conversion done?
+ ADCValue = ADC1BUF0;
+ if(ADCValue>2500)LED=1;
+ else LED=0;
+ 
+ d=(float)ADCValue/4095;
+ //d=0.65;
+ d=d*2;
+    if(d>=1){
+        d=d-1;
+        if(d*800<600)PWM_update((int)(d*800),800);
+        else PWM_update(600,800);
+   
+    }else PWM_update(0,(int)(d*800));
+    
+  
+}
+
+
+void PWM_Init()
+{
+    TRISBbits.TRISB12=0;
+    TRISBbits.TRISB14=0;
+    // RB14 and Rb12
+    PORTB = 0x00; // clear the outputs 
+ 
+    PTCONbits.PTOPS = 1; // PWM timer post-scale
+    PTCONbits.PTCKPS = 0; // PWM timer pre-scale
+    PTCONbits.PTMOD = 2; // PWM operates in Up-down Mode continuously 
+    //--> interupt each time wwe get to zero
+ 
+    PTMR = 0; // PWM counter value, start at 0
+    PTPER = 799; // PWM Timebase period
+    
+    PWMCON1bits.PMOD2 = 0; // PWM in complimentary mode
+    PWMCON1bits.PMOD1 = 0; // PWM in complimentary mode
+    PWMCON1bits.PEN2H = 1; // PWM High pin is enabled
+    PWMCON1bits.PEN1H = 1; // PWM High pin is enabled
+   
+   
+    DTCON1bits.DTAPS = 0;  //DeadTime pre-scaler = Tcy
+    DTCON1bits.DTA = 0;   //DeadTime value for 4 us. 
+
+    PDC1 = p1; // PWM#1 Duty Cycle register (11-bit)
+    PDC2 = p2; // PWM#2 Duty Cycle register (11-bit)
+
+    PTCONbits.PTEN = 1; // Enable PWM Timerbase!
+}
+void PWM_update(int pwm1,int pwm2){
+PTCONbits.PTEN=0;
+PDC1 = pwm1;
+PDC2 = pwm2;
+PTCONbits.PTEN=1;
+}
+
+void SetupOSC(){
+
+    //FCY = 40 Mhz
+    
+PLLFBD = 41;// M = 43
+CLKDIVbits.PLLPOST = 0; // N1 = 2
+CLKDIVbits.PLLPRE = 0; // N2 = 2
+
+__builtin_write_OSCCONH(0x03);
+__builtin_write_OSCCONL(0x01);
+// Wait for clock switch to occur
+while (OSCCONbits.COSC != 0b011);
+while(OSCCONbits.LOCK !=1); // Wait for PLL to lock
+
+}
+
+//read each 5khz
+void ADC_Setup(){
+
+ TRISAbits.TRISA0=1;    
+ AD1PCFGL = 0xFFFE;// all PORTA = Digital; RA0 = analog
+ //10 bit
+ //AD1CON1 = 0x00E0; 
+ //12 bit
+ AD1CON1 = 0x04E0; 
+ 
+ AD1CHS0= 0x0000; // Connect RA0/AN0 as CH0 input ..
+ 
+ AD1CSSL = 0;
+ AD1CON3 = 0x1F02; // Tsampling = 2 Tad
+ AD1CON2 = 0;
+ AD1CON1bits.ADON = 1; // turn ADC ON
+ 
+}
+
---
+++ PWM.c
@@ -0,0 +1,75 @@
+/*
+ * File:   main.c
+ * Author: Nazim
+ *
+ * Created on 18 juin 2018, 11:58
+ */
+#include <p33fj32mc204.h>
+#include "xc.h"
+#include <libpic30.h> 
+
+
+_FOSCSEL(FNOSC_FRCPLL);			// Start with FRC will switch to Primary (XT, HS, EC) Oscillator with PLL
+_FOSC(FCKSM_CSDCMD & POSCMD_NONE);	// Clock Switching disabled 
+_FBS (BSS_NO_FLASH & BWRP_WRPROTECT_OFF);
+_FWDT (FWDTEN_OFF);
+_FGS (GSS_OFF & GCP_OFF & GWRP_OFF);
+_FPOR (PWMPIN_ON & HPOL_ON & LPOL_ON & FPWRT_PWR128);
+_FICD (ICS_PGD1 & JTAGEN_OFF);
+
+//#define FCY 39680028
+#define FCY 40000000
+
+void PWM_Init()
+{
+    TRISB = 0x00; // make sure PWM pins are set to be outputs
+    PORTB = 0x00; // clear the outputs 
+ 
+    PTCONbits.PTOPS = 1; // PWM timer post-scale
+    PTCONbits.PTCKPS = 0; // PWM timer pre-scale
+    PTCONbits.PTMOD = 2; // PWM operates in Up-down Mode continuously
+ 
+    PTMR = 0; // PWM counter value, start at 0
+ 
+    PTPER = 19999; // PWM Timebase period
+ 
+    PWMCON1bits.PMOD3 = 0; // PWM in complimentary mode
+    PWMCON1bits.PMOD2 = 0; // PWM in complimentary mode
+    PWMCON1bits.PMOD1 = 0; // PWM in complimentary mode
+    PWMCON1bits.PEN3H = 1; // PWM High pin is enabled
+    PWMCON1bits.PEN2H = 1; // PWM High pin is enabled
+    PWMCON1bits.PEN1H = 1; // PWM High pin is enabled
+    PWMCON1bits.PEN3L = 1; // PWM Low pin enabled (direction control later?)
+    PWMCON1bits.PEN2L = 1; // PWM Low pin enabled (direction control later?)
+    PWMCON1bits.PEN1L = 1; // PWM Low pin enabled (direction control later?)
+ 
+    DTCON1bits.DTAPS = 0;  //DeadTime pre-scaler
+    DTCON1bits.DTA = 59;   //DeadTime value for 4 us. 
+ 
+
+    PDC1 = 19999; // PWM#1 Duty Cycle register (11-bit)
+    PDC2 = 19999; // PWM#2 Duty Cycle register (11-bit)
+    PDC3 = 19999; // PWM#3 Duty Cycle register (11-bit)
+ 
+    PTCONbits.PTEN = 1; // Enable PWM Timerbase!
+}
+
+void OSC_Init(){
+	
+    PLLFBD =  33;		        // M=45
+	CLKDIVbits.PLLPOST = 0;		// N1=2
+	CLKDIVbits.PLLPRE = 0;		// N2=2
+
+   // OSCCONbits.OSWEN=1;
+	while (OSCCONbits.COSC != 0b001);
+    while(OSCCONbits.LOCK!=1);
+} 
+   
+void main(void) {
+    
+   OSC_Init();
+   PWM_Init();
+   
+    while(1);
+       
+}
